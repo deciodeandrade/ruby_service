7055553d85c211c7d3ee9338e2cb4c3b3b5276c4
Décio <deciocavalcantedeandrade@gmail.com>
2024-10-23 11:19:58 -0300

    Refatorando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      post 'mean', to: 'calculations#mean'
-      post 'median', to: 'calculations#median'
-      post 'sum', to: 'calculations#sum'
-      post 'minimum', to: 'calculations#minimum'
-      post 'maximum', to: 'calculations#maximum'
-      post 'standard_deviation', to: 'calculations#standard_deviation'
-      post 'count', to: 'calculations#count'
-      post 'variance', to: 'calculations#variance'
+    resources :calculations, only: [] do
+      collection do
+        post :mean
+        post :median
+        post :sum
+        post :minimum
+        post :maximum
+        post :standard_deviation
+        post :count
+        post :variance
+      end
     end
   end
 end
